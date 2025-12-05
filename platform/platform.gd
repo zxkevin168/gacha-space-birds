b43371c6f45b7f3b4b8af3381e43949e3867f0a8
Edward Tsai <edward.tsai.0091+github@gmail.com>
2025-12-05 00:58:17 -0800

    simple platform spawner

--- platform/platform.gd
+++ platform/platform.gd
@@ -1,14 +1,14 @@
 extends StaticBody2D
 
+var speed = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
     pass
+
+
+func _process(delta: float) -> void:
+    var velocity = Vector2(-1, 0) * speed * delta
+    position += velocity
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
